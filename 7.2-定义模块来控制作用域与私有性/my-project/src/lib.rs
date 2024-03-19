16e552b83e3001ebf816f8b93df1a6881d163a16
yikeJason <18215513386@163.com>
2024-03-19 23:39:11 +0800

    模块路径笔记

--- 7.2-定义模块来控制作用域与私有性/my-project/src/lib.rs
+++ 7.2-定义模块来控制作用域与私有性/my-project/src/lib.rs
@@ -1,11 +1,23 @@
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
+//         fn seat_at_table() {}
+//     }
+//     mod serving {
+//         fn take_order() {}
+//         fn serve_order() {}
+//         fn take_payment() {}
+//     }
+// }
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist(); // 绝对路径
+
+    front_of_house::hosting::add_to_waitlist(); // 相对路径
 }
